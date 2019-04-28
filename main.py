5985f648528c437443b767077e23f2e52dec0980
Patryk Sladek <sladzio995@gmail.com>
2019-04-28 18:07:42 +0200

    *Added visualization of terrorist attacks

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import networkx as nx
 import datetime
 from heapq import nsmallest
 import math
-import matplotlib.pyplot as plt
+from pyvis.network import Network
 
 
 def create_date(row):
@@ -19,14 +19,15 @@ def distance(row1, row2):
 
 
 data = pd.read_csv("data.csv", encoding="ISO-8859-1")
-dates_and_kills = data[['iyear', 'imonth', 'iday', 'nkill', 'city']].dropna()
+dates_and_kills = data[['iyear', 'imonth', 'iday', 'nkill', 'city', 'country_txt']].dropna()
 dates_and_kills = dates_and_kills.loc[
     (dates_and_kills['iyear'] >= 2017) & (dates_and_kills['nkill'] > 0) & (dates_and_kills['imonth'] > 0) & (
             dates_and_kills['iday'] > 0)].head(1000)
 G = nx.Graph()
+labels = {}
 for index, row in dates_and_kills.iterrows():
-
     distances = {}
+    G.add_node(index, title=row['city'])
     for index2, row2 in dates_and_kills.iterrows():
         if not G.has_edge(index, index2):
             if index != index2:
@@ -34,7 +35,13 @@ for index, row in dates_and_kills.iterrows():
     smallest = nsmallest(3, distances, key=distances.get)
     for neighbour in smallest:
         G.add_edge(index, neighbour)
+    labels[index] = row['city'] + ", " + row['country_txt'] + ' on ' + create_date(row).strftime("%d %b, %Y")
+g = Network(height=1200, width=1200, notebook=True, bgcolor="#222222", font_color="white")
+g.toggle_hide_edges_on_drag(False)
+g.from_nx(G)
 
-nx.draw(G)
+for node in g.nodes:
+    node["title"] = labels[node["id"]]
+
+g.show("ex.html")
 nx.write_edgelist(G, "graph.txt")
-plt.show()
